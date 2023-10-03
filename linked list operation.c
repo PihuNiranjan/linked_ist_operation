1928723a740009a78004514cd4c343e5ba370d9e
Piyush niranjan <89120915+PihuNiranjan@users.noreply.github.com>
2023-10-04 01:12:23 +0530

    linked list operation.c

--- linked list operation.c
+++ linked list operation.c
@@ -1,16 +1,7 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 
-int
-main ()
+int main ()
 {
   printf ("My first linked list program in c");
 
